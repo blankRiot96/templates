a015ab466b9e29758b75c5af8923ac95c0858241
Axis <77634274+blankRiot96@users.noreply.github.com>
2023-11-24 19:41:31 +0530

    updated basic to display fps

--- templates/pg-basic/main.py
+++ templates/pg-basic/main.py
@@ -2,12 +2,17 @@ import pygame
 
 pygame.init()
 win = pygame.display.set_mode((800, 450))
+clock = pygame.Clock()
+
 
 while True:
+    dt = clock.tick(60) / 1000
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             raise SystemExit
+
+    pygame.display.set_caption(f"{clock.get_fps():.0f}")
 
     win.fill("black")
     pygame.display.update()
